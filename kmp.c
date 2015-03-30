643a36074487f860994483f25cddb56183dd8da1
yadongshao <348829719@qq.com>
2015-03-30 12:46:53 +0800

    Update kmp.c

--- kmp.c
+++ kmp.c
@@ -1,3 +1,6 @@
+//在进行子字符串（从0记）i位对应的next数组值求解的时候  就是对比i-1位的字符与i-1位对应的next[i-1]的值所对应的字符
+//进行对比  如果相同则 next[i]=next[i-1]+1;    如果不同则是将i-1位的字符与next[i-1]对应的字符的对应的next值的对应字符
+//相互比较  重复过程，最终都没有匹配的话  就是next值为-1  这时就将这个值加1  
 void GetNext(char*key,int *next){
 	int length=strlen(key);
 	int i=0;
